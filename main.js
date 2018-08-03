cf4a8b9e542f8afa5dd7cb830ed9b54533045a48
Ben <ben.harder2@gmail.com>
2018-08-03 11:44:12 -0700

    added files. Copying files works.

---
+++ main.js
@@ -0,0 +1,51 @@
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const fs = require('fs');
+
+let win;
+
+function createWindow()
+{
+    win = new BrowserWindow({ width: 650, height: 550 });
+
+    win.loadFile('index.html');
+
+    win.webContents.openDevTools();
+
+    win.on('closed', () =>
+    {
+        win = null;
+    });
+
+    Menu.setApplicationMenu(null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => 
+{
+    if (process.platform !== 'darwin')
+    {
+        app.quit();
+    }
+});
+
+app.on('activate', () => 
+{
+    if (win === null)
+    {
+        createWindow();
+    }
+});
+
+ipcMain.on('source:submit, destination:submit', function (e, source, destination)
+{
+    copy(source, destination);
+});
+
+function copy(source, destination)
+{
+    fs.copyFileSync(source, destination, (err) =>
+    {
+        if (err) throw err;
+    });
+}
